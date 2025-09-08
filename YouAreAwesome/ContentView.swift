bf45bb1fa9cadf1e76ba14b3237db57e893f316e
chen-domi <cchendominic@gmail.com>
2025-09-08 08:25:10 -0400

    Incorporate while loop so that images and messages should not duplicate

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -10,8 +10,9 @@ import SwiftUI
 struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
-    @State private var imageNumber = 0
-    @State private var messageNumber = 0
+    @State private var lastMessageNumber = -1
+    @State private var lastImageNumber = -1
+
     
     var body: some View {
         
@@ -46,8 +47,24 @@ struct ContentView: View {
                                 "You make me smile!",
                                 "When the Genius Bar needs help, They Call You!"]
                 
-                message = messages[Int.random(in: 0...messages.count - 1)]
-                imageName = "image\(Int.random(in: 0...9))"
+                var messageNumber = Int.random(in: 0...messages.count - 1)
+                while messageNumber == lastMessageNumber {
+                    messageNumber = Int.random(in: 0...messages.count - 1)
+                }
+                message = messages[messageNumber]
+                lastMessageNumber = messageNumber
+                
+                
+                var imageNumber = Int.random(in: 0...9)
+                while imageNumber == lastImageNumber {
+                    imageNumber = Int.random(in: 0...9)
+                }
+                imageName = "image\(imageNumber)"
+                lastImageNumber = imageNumber
+                
+                
+//                message = messages[Int.random(in: 0...messages.count - 1)]
+//                imageName = "image\(Int.random(in: 0...9))"
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
